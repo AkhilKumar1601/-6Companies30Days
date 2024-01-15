32ec71bf6d579b30fe4491243bba2da15771ca0b
AkhilKumar1601 <akhilchaudhary1601@gmail.com>
2024-01-15 18:28:54 +0530

    DAY14 PROBLEMS  SOLVED

---
+++ 14.1 VerifyPreOrderSerializationOfBinaryTree(331)/verify.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<sstream>
+#include<stack>
+using namespace std;
+
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+        stringstream s(preorder);
+        stack<string> st;
+        string str;
+        
+        while(getline(s, str, ',')){
+            if(st.empty() || str != "#") st.push(str);
+            else{
+                while(!st.empty() && st.top() == "#"){
+                    st.pop();
+                    if(st.empty()) return false;
+                    else st.pop();
+                }
+                
+                st.push(str);
+            }
+        }
+        
+        return st.size() == 1 && st.top() == "#";
+    } 
+};
