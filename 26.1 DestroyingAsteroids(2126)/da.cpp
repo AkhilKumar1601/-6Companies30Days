767e22a382d431900a1a2210d8cedab1495fec64
AkhilKumar1601 <akhilchaudhary1601@gmail.com>
2024-01-27 06:54:06 +0530

    DAY 26 IS COMPLETED

---
+++ 26.1 DestroyingAsteroids(2126)/da.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(),asteroids.end());
+        long long mass1= mass;
+        for (int i=0; i<asteroids.size(); i++){
+            if (mass1>=asteroids[i])  mass1+= asteroids[i];
+            else return 0;
+        }
+        return 1;
+    }
+};
